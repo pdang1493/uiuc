be86cb1f89c1567184954bed3f1f31e355113c85
Phu Dang <phudang@MacBook-Pro.local>
2025-01-28 17:48:42 -0600

    Initial commit for server and stress test scripts

---
+++ serve.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import subprocess
+import socket
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def stress_cpu():
+    # Start stress_cpu.py in a non-blocking manner
+    subprocess.Popen(['python', 'stress_cpu.py'])
+    return 'CPU stress test started', 200
+
+@app.route('/', methods=['GET'])
+def get_private_ip():
+    # Get the private IP address
+    hostname = socket.gethostname()
+    private_ip = socket.gethostbyname(hostname)
+    return private_ip
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
